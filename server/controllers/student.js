0e9710a8825a27f91f10a9adf3e4209c5936bc62
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-10-11 14:10:14 +0300

    do students registeration api

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,14 +1,17 @@
 
 const Student=require('../models/student')
+
+
 const registerStudent=async(req,res)=>{  
 
     try{
-        const fields =req.body
-        const newStudent= await Student.create(fields)
-        res.status(200).json(news)
+        const student =req.body
+        const newStudent= await Student.create(student)
+        if(!newStudent) return res.status(400).send('cant create')
+        res.status(200).json(newStudent)
         
     }catch(error){
-        res.json({'the error':error})
+        res.status(500).json({'the error':error})
     }
 }
 
@@ -16,43 +19,49 @@ const registerStudent=async(req,res)=>{
 const getStudents=async(req,res)=>{
     try{
         const student=await Student.find()
-        if(!student) res.send('faild')
-        res.json(student)    
+        if(!student) return res.send('faild')
+        res.status(200).json(student)    
     }catch(err){
-        res.send(err)
+        res.status(500).send(err)
     }
     
 }
 
 
 const getOneStudent=async(req,res)=>{
-    const {id}=req.params
-    const student=await Student.findById(id)
-    if(!student) res.send('faild')
-    res.json(student)
+    try{
+        const {id}=req.params
+        const student=await Student.findById(id)
+        if(!student) return res.status(400).send('failed')
+        res.status(200).json(student)
+    
+    }catch(error){
+        res.status(500).send(error)
+    }
     
 }
 
 const deleteStudent=async(req,res)=>{
     try{
       const student=  await Student.findByIdAndDelete(req.params.id)
-      res.json(student,'deleted')
+      if(!student) return res.status(400).send('faild')
+      res.status(200).json({msg:'deleted', student})
         
     }catch(error){
-        res.json(error)
+        res.status(500).json(error)
     }
 }
 
 
 const updateStudent=async(req,res)=>{
     try{
-        const fields=req.body
+        const datas=req.body
         const {id}=req.params
-        const student= await Student.findByIdAndUpdate(fields,id,{new:true})
-         if(!student) res.send('failed')
-        res.json({'updated user': student})
+        const student= await Student.findByIdAndUpdate(id,datas,{new:true})
+         if(!student) return res.status(400).send('failed')
+        res.status(200).json( student)
     }catch(error){
-        res.json(error)
+        res.status(500).json(error.message)
     }
   
 
