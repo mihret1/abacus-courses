8a119e8df95d6b37834e0fae9cb2f89f5b011137
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-10-11 13:13:14 +0300

    finish interested student api

--- server/controllers/interested.js
+++ server/controllers/interested.js
@@ -6,19 +6,20 @@ const signupInterestedStudent=async(req,res)=>{
       try{
         const student = req.body
         const newStudent= await InterestedStudent.create(student)
-        if(!newStudent) res.send('cant create')
-        res.status(204).json(newStudent)    
+        if(!newStudent) return res.send('can`t create')
+        res.status(200).json(newStudent)    
       }catch(error){
-        res.send(error)
+        res.status(500).send(error)
       }
 
 }
 
 const getInterestedStudents=async(req,res)=>{
     try{
+      
         const students=await InterestedStudent.find()
-        if(!students) res.send('there is no')
-         res.json(students)   
+        if(!students) return res.send('there is no')
+         res.status(200).json(students)   
     }catch(err){
         res.send(err)
     }
@@ -26,11 +27,13 @@ const getInterestedStudents=async(req,res)=>{
 
 const deleteInterestedStudent=async(req,res)=>{
     try{
+        const todelete=await InterestedStudent.findById(req.params.id)
+        if(!todelete) return res.status(400).send('interested  doenst exit')
         const student=await InterestedStudent.findByIdAndDelete(req.params.id)
-        if(!student) res.send('faild')
-        res.send('deleted')   
+        if(!student) return res.send('faild to delete')
+        res.status(200).send('deleted')   
     }catch(error){
-        res.send(error)
+        res.status(500).send(error)
     }
 }
 
